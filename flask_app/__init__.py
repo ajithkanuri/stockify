c473e733824662faabdcf1db02c2d6eb8f6e5312
Ankur Mishra <2018ankurm@gmail.com>
2021-05-10 15:20:41 -0400

    revert changes

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -45,8 +45,8 @@ def create_app(test_config=None):
     app.config['MAIL_SERVER']='smtp.gmail.com'
     app.config["MAIL_USE_TLS"]= False
     app.config[ "MAIL_USE_SSL"]= True
-    app.config['MAIL_PASSWORD'] = os.getenv("MAIL_PASSWORD")
-    app.config['MAIL_USERNAME'] = os.getenv("MAIL_USERNAME")
+    app.config['MAIL_PASSWORD'] = "Stockify1!"
+    app.config['MAIL_USERNAME'] = 'confirm.stockify.code@gmail.com'
     app.config['MAIL_PORT'] = 465 
     
     db.init_app(app)
